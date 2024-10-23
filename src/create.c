4b4002e57a27388bbfed2bab7517d38241c6acbc
lolguy91 <retek1544@gmail.com>
2024-10-23 13:51:59 +0200

    made creating work

--- src/create.c
+++ src/create.c
@@ -1,8 +1,53 @@
 #include "create.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <sys/stat.h>
 
 bool create(char* directory) {
-    printf("Creating %s\n", directory);
+
+    if (directory == NULL) {
+        return false;
+    }
+
+    //make the directory
+    if (mkdir(directory, 0777) != 0) {
+        return false;
+    }
+
+    //make the src directory
+    char src[1024];
+    strcpy(src, directory);
+    strcat(src, "/src");
+    if (mkdir(src, 0777) != 0) {
+        return false;
+    }
+
+    char* main = strcat(src, "/main.c");
+    FILE* file = fopen(main, "w+");
+    if(file == NULL) {
+        printf("Failed to create file: %s\n", main);
+        return false;
+    }
+
+    fprintf(file, "#include <stdio.h>\n\nint main(int argc, char **argv) {\n");
+    fprintf(file, "\tprintf(\"Hello World!\\n\");\n");
+    fprintf(file, "\treturn 0;\n");
+    fprintf(file, "}\n");
+
+    fclose(file);
+
+    //create a file in directory named "drvdesc"
+    char* drvdesc = strcat(directory, "/.drvdesc");
+    file = fopen(drvdesc, "w+");
+    if(file == NULL) {
+        printf("Failed to create file: %s\n", drvdesc);
+        return false;
+    }
+    fprintf(file, "This is a driver for %s\n", directory);
+    fclose(file);
+
     return true;
 }
