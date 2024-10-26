871daf9b013eee44e1938eb4039e39c88e151cd1
lolguy91 <retek1544@gmail.com>
2024-10-26 15:11:45 +0200

    changed the default project to be better

--- src/create.c
+++ src/create.c
@@ -31,10 +31,43 @@ bool create(char* directory) {
         return false;
     }
 
-    fprintf(file, "#include <stdio.h>\n\nint main(int argc, char **argv) {\n");
-    fprintf(file, "\tprintf(\"Hello World!\\n\");\n");
-    fprintf(file, "\treturn 0;\n");
-    fprintf(file, "}\n");
+    fprintf(file,"#include <core.h>\n");
+    fprintf(file,"#include <metalanguages/misc/storage.h>\n");
+    fprintf(file,"\n");
+    fprintf(file,"metalang_storage_t* storage;\n");
+    fprintf(file,"uint32_t devide_id = 0;\n");
+    fprintf(file,"\n");
+    fprintf(file,"sdev_ident_t storage_ident_callback(adi_device_t* dev) {\n");
+    fprintf(file,"	return {\n");
+    fprintf(file,"		.size = sizeof(\"Hello, world!\"),\n");
+    fprintf(file,"		.sector_size = 1,\n");
+    fprintf(file,"		.read_only = true\n");
+    fprintf(file,"	};\n");
+    fprintf(file,"}\n");
+    fprintf(file,"\n");
+    fprintf(file,"uint32_t storage_transact_callback(bool write,uint32_t offset,uint32_t count,void* buffer){\n");
+    fprintf(file,"\n");
+    fprintf(file,"	if(write)\n");
+    fprintf(file,"		return 0;\n");
+    fprintf(file,"	if (offset + count > sizeof(\"Hello, world!\"))\n");
+    fprintf(file,"		return 0;\n");
+    fprintf(file,"\n");
+    fprintf(file,"	char* hw = \"Hello, world!\";\n");
+    fprintf(file,"\n");
+    fprintf(file,"	memcpy(buffer, hw + offset,count);\n");
+    fprintf(file,"\n");
+    fprintf(file,"	// recieving the transaction_done event before the storage_transact call finishing is a perfectly normal scenario\n");
+    fprintf(file,"	// and programs should account for it\n");
+    fprintf(file,"	storage->signal_transaction_done(device_id,1);\n");
+    fprintf(file,"\n");
+    fprintf(file,"	return 1;\n");
+    fprintf(file,"}\n");
+    fprintf(file,"\n");
+    fprintf(file,"int _init() {\n");
+    fprintf(file,"	metalanguage_t langs[] = {storage->new(storage_ident_callback,storage_transact_callback)};\n");
+    fprintf(file,"	device_id = register_devices(storage,1);\n");
+    fprintf(file,"	return 0;\n");
+    fprintf(file,"}\n");
 
     fclose(file);
 
@@ -47,16 +80,13 @@ bool create(char* directory) {
     }
     fprintf(file,"name: Lonely Driver\n");
     fprintf(file,"author: Lolguy91\n");
-    fprintf(file,"#cross-arch: aarch64\n");
-    fprintf(file,"regions:\n");
-    fprintf(file,"\tmain:\n");
-    fprintf(file,"\t\tpermissions: low\n");
-    fprintf(file,"\trequire_kernel: false\n");
-    fprintf(file,"\tsrc_dir: src\n");
-    fprintf(file,"\tuses:\n");
-    fprintf(file,"\t\t\t- x86\n");
-    fprintf(file,"\t\timplements:\n");
-    fprintf(file,"\t\t\t- timekeeper");
+    fprintf(file,"#commented out stuff bellow is default\n");
+    fprintf(file,"#cross-arch: x86\n");
+    fprintf(file,"#permissions: none\n");
+    fprintf(file,"#require_kernel: false\n");
+    fprintf(file,"src_dir: src\n");
+    fprintf(file,"implements:\n");
+    fprintf(file,"\t- misc_storage\n");
     fclose(file);
 
     return true;
